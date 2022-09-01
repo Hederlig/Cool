bb88353c4fc1e02c32a939e51e8c0299ba0906d3
Oscar <oscar.hederlig@gmail.com>
2022-09-01 14:41:02 +0200

    New changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <div>
+        <Container>
+        <h1>What's your Tone of Fashion?</h1>
+      <Row>
+        <Col sm={6}>Bild 1</Col>
+      
+        <Col  sm={6}>Bild 2</Col>
+       
+        <Col sm={6} >Bild 3</Col>
+        <Col sm={6}>Bild 4</Col>
+        <Col sm={6}>Bild 5</Col>
+        <Col sm={6}>Bild 6</Col>
+      </Row>
+    </Container>
+ 
+
+      <footer>
+        <h5>This is footer</h5>
+      </footer>
+      </div>
   );
 }
 
