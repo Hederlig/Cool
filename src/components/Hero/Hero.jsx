3ccbbbd4d6264133d6d22870129993cf7371b202
Oscar <oscar.hederlig@gmail.com>
2022-09-03 13:15:48 +0200

    Main & Hero got more code. need some styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,23 +2,50 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import "./Hero.css";
+
 const Hero = () => {
-    return (
-      <div>
-        <Container>
-          <h1>This is Hero</h1>
-          <Row>
-            <Col sm={6}>
-              <img src="https://via.placeholder.com/240x320" alt="Tshirt" />
-              Bild 1
-            </Col>
-     
-  
-            
-          </Row>
-        </Container>
-      </div>
-    );
-  };
-  
-  export default Hero;
+  return (
+    <div>
+      <Container>
+        <h1>This is Hero</h1>
+        <Row>
+          <Col sm={6}>
+            <div
+              style={{
+                width: "505px",
+                height: "320px",
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundImage: `url("https://via.placeholder.com/505x320")`,
+              }}
+            >
+              <div className="Buttons">
+                <p className="heroText">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Maiores eveniet eaque impedit expedita iusto vero dolores
+                  mollitia quibusdam voluptatem aliquam aut, hic labore ut
+                  numquam. Facilis nihil soluta ut consequuntur?s
+                </p>
+                <Row>
+                  <Col>
+                    <Button variant="dark">Jeans</Button>
+                  </Col>
+                  <Col>
+                    <Button variant="dark">Jeans</Button>
+                  </Col>
+                  <Col>
+                    <Button variant="dark">Jeans</Button>
+                  </Col>
+                </Row>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,48 +2,64 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import "./Main.css";
 
 const Main = () => {
   return (
     <div>
       <Container>
-        <h1>What's your Tone of Fashion?</h1>
+        <Col>
+        <div className="topButtons">
+          <Button variant="primary" size="lg">
+            Nyheter{" "}
+          </Button>
+          <Button variant="primary" size="lg">
+            REA{" "}
+          </Button>
+          </div>
+        </Col>
         <Row>
           <Col sm={6}>
             <img src="https://via.placeholder.com/240x320" alt="Tshirt" />
-            <p>Vad är det för produkt?</p>
+            <p>Vit T-tröja</p>
           </Col>
           <Col sm={6}>
             <img src="https://via.placeholder.com/240x320" alt="Tshirt" />
-            <p>Vad är det för produkt?</p>
+            <p>Svart Hoodie</p>
           </Col>
           <Col sm={6}>
             <img src="https://via.placeholder.com/240x320" alt="Tshirt" />
-            <p>Vad är det för produkt?</p>
+            <p>Stentvättade Jeans</p>
           </Col>
           <Col sm={6}>
             <img src="https://via.placeholder.com/240x320" alt="Tshirt" />
-            <p>Vad är det för produkt?</p>
+            <p>Rosa Skor</p>
           </Col>
           <Col sm={6}>
             <img src="https://via.placeholder.com/240x320" alt="Tshirt" />
-            <p>Vad är det för produkt?</p>
+            <p>Lila Kjol</p>
           </Col>
           <Col sm={6}>
             <img src="https://via.placeholder.com/240x320" alt="Tshirt" />
-            <p>Vad är det för produkt?</p>
+            <p>Läder Jacka</p>
           </Col>
           <Col sm={6}>
             <img src="https://via.placeholder.com/240x320" alt="Tshirt" />
-            <p>Vad är det för produkt?</p>
+            <p>Svarta Skor</p>
           </Col>
           <Col sm={6}>
             <img src="https://via.placeholder.com/240x320" alt="Tshirt" />
-            <p>Vad är det för produkt?</p>
+            <p>Gant Piké</p>
           </Col>
 
-
-          
+          <Col sm={12}>
+            <div className="moreButton">
+              <Button variant="primary" size="lg">
+                Visa mer
+              </Button>
+            </div>
+          </Col>
         </Row>
       </Container>
     </div>
